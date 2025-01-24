3dad4cafdc927ba839ecac2c493a86471dabe08c
octaviusthe3rd <a.chagaresa@alustudent.com>
2025-01-24 22:54:21 +0200

    errors

--- models/base_model.py
+++ models/base_model.py
@@ -25,37 +25,18 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            valid_attrs = ['id', 'created_at', 'updated_at', '__class__']
+            # Dynamically set attributes from kwargs
+            for key, value in kwargs.items():
+                if key == "created_at" and isinstance(value, str):
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == "updated_at" and isinstance(value, str):
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
 
-            if hasattr(self.__class__, '__table__'):
-                valid_attrs.extend([
-                    col.name for col in self.__class__.__table__.columns
-                ])
-                valid_attrs.append('_sa_instance_state')
-
-            invalid_keys = [k for k in kwargs if k not in valid_attrs]
-            if invalid_keys:
-                error_msg = (
-                    f"Invalid attribute(s): {', '.join(invalid_keys)} "
-                    f"for {self.__class__.__name__}"
-                )
-                raise KeyError(error_msg)
-
-            self.id = kwargs.pop('id', str(uuid.uuid4()))
-            self.created_at = kwargs.pop('created_at', datetime.now())
-            self.updated_at = kwargs.pop('updated_at', datetime.now())
-
-            if isinstance(self.created_at, str):
-                self.created_at = datetime.strptime(
-                    self.created_at, '%Y-%m-%dT%H:%M:%S.%f'
-                )
-            if isinstance(self.updated_at, str):
-                self.updated_at = datetime.strptime(
-                    self.updated_at, '%Y-%m-%dT%H:%M:%S.%f'
-                )
-
-            kwargs.pop('__class__', None)
-            self.__dict__.update(kwargs)
+            # Set default values for id, created_at, and updated_at if not provided
+            self.id = kwargs.get('id', str(uuid.uuid4()))
+            self.created_at = kwargs.get('created_at', datetime.now())
+            self.updated_at = kwargs.get('updated_at', datetime.now())
 
     def __str__(self):
         """Returns a string representation of the instance"""
