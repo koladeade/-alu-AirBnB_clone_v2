463e134134a3a575d3c064c51d73984d4635f4f6
octaviusthe3rd <a.chagaresa@alustudent.com>
2025-01-24 20:02:12 +0200

    errors

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 import os
 
+
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
